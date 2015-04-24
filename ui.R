336d52f9509692eac02391d368fed79be635e80d
KJX <elan_shibuya_fx@yahoo.com.sg>
2015-04-25 00:12:33 +0800

    amended ui.R

--- ui.R
+++ ui.R
@@ -6,7 +6,7 @@ shinyUI(fluidPage(
   # Application title
   titlePanel("Central Limit Theorem Demonstration"),
   br(),
-  p("This application aims to quickly demonstrate the basic idea of Central Limit Theorem using the exponential distribution. The CLT states that the distribution of averages becomes that of a standard normal as the sample size of the simulation increases and that the distribution will be centered at the population mean. Each sample in this application is the average of 40 exponentials. The lamda rate is 0.2. Population mean is 5, which is depicted by the Red line."),
+  p("This application aims to quickly demonstrate the basic idea of Central Limit Theorem using the exponential distribution. The CLT states that the distribution of averages becomes that of a standard normal as the sample size of the simulation increases and that the distribution will be centered at the population mean. Each sample in this application is the average of 40 exponentials. The lambda rate is 0.2. Population mean is 5, which is depicted by the Red line."),
   p("The idea here is to use the widget below to change the number of simulations so as to see the changes in the distribution plot and see how it converges to the population mean and taking the shape of a normal distribution."),
   br(),
   div("The number of simulations and colour of the histogram is configurable below. Note that the computation time increases when the number of simulation increases. Change in colour of the histogram would also require a re-computation.", style = "color:blue"),
